85bf15d8e921d754c42d27cbfc6c89145fc1f0ef
Edoardo <edoardodemarchi@gmail.com>
2016-02-22 21:13:20 +0100

    Added ImageGridAdapter

--- app/src/main/java/com/example/edoardo/moviesapp/MainActivityFragment.java
+++ app/src/main/java/com/example/edoardo/moviesapp/MainActivityFragment.java
@@ -1,22 +1,127 @@
 package com.example.edoardo.moviesapp;
 
-import android.support.v4.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
-    public MainActivityFragment() {
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.sample_0,
+            R.drawable.sample_1,
+            R.drawable.sample_2,
+            R.drawable.sample_3,
+            R.drawable.sample_4,
+            R.drawable.sample_5,
+            R.drawable.sample_6,
+            R.drawable.sample_7,
+            R.drawable.sample_0,
+            R.drawable.sample_1,
+            R.drawable.sample_2,
+            R.drawable.sample_3,
+            R.drawable.sample_4,
+            R.drawable.sample_5,
+            R.drawable.sample_6,
+            R.drawable.sample_7,
+            R.drawable.sample_0,
+            R.drawable.sample_1,
+            R.drawable.sample_2,
+            R.drawable.sample_3,
+            R.drawable.sample_4,
+            R.drawable.sample_5,
+            R.drawable.sample_6,
+            R.drawable.sample_7
+    };
+
+    // ImageListAdapter oImageAdapter;
+
+
+    public MainActivityFragment()
+    {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+                             Bundle savedInstanceState)
+    {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        GridView gridview = (GridView) rootView.findViewById(R.id.gridView_movies);
+
+
+        ImageGridAdapter oTest = new ImageGridAdapter(getContext());
+        gridview.setAdapter(oTest);
+
+        return rootView;
     }
+
+
+
+
+
+
+
+
+    public class ImageGridAdapter extends BaseAdapter {
+        private Context mContext;
+        private LayoutInflater inflater;
+
+        public ImageGridAdapter(Context c) {
+            mContext = c;
+            inflater = LayoutInflater.from(mContext);;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            ImageView imageView;
+
+
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                convertView = inflater.inflate(R.layout.image_view_movies, parent, false);
+                imageView = (ImageView) convertView.findViewById(R.id.item_imageview_movies);
+
+              //*  imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(250, 250));
+               // imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+               // imageView.setPadding(8, 8, 8, 8);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+        }
+
+
+    }
+
+
+
+
+
 }
